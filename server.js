adc9eaa665a26368cd21154f9f8cf980af21498b
Sachin-pandey13 <aatrox1302@gmail.com>
2025-06-23 16:21:18 +0530

    server.js

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ io.on('connection', socket => {
     io.emit('user-count', io.engine.clientsCount);
   });
 });
-
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
